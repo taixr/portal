5b9cf91c3a1f4181432a096d004a07323b3d032f
alanhou <info@iscbj.com>
2024-05-17 21:34:55 +0800

    第六章 p5.js中的更多变量

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -49,7 +49,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/taixr/portal/tree/main/p',
+            'https://github.com/taixr/portal/tree/main/',
         },
         theme: {
           customCss: './src/css/custom.css',
